f45befd6079b951bce08a74295dcac331125ede9
kisstest <kisstest2@gmail.com>
2020-06-27 15:43:59 +0900

    Revert "[build error #22](https://github.com/gumpcha/good-reads-clone/issues/22)"
    This reverts commit bc69962bc9494d385cad9d302544c635f16fbb93.

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
 	.BundleAnalyzerPlugin;
-const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
 	configureWebpack: {
@@ -22,19 +21,6 @@ module.exports = {
 				chunks: 'all',
 			},
 			usedExports: true,
-			minimize: true,
-			minimizer:
-				process.env.NODE_ENV === 'production'
-					? [
-							new TerserPlugin({
-								terserOptions: {
-									ecma: 6,
-									compress: { drop_console: true },
-									output: { comments: false, beautify: false },
-								},
-							}),
-					  ]
-					: [],
 		},
 		plugins: [
 			new CleanWebpackPlugin(),
