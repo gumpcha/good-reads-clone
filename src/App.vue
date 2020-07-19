879c715870699f5ef23071c55ec4296440bb83d3
kisstest <kisstest2@gmail.com>
2020-07-19 15:58:16 +0900

    ....

--- src/App.vue
+++ src/App.vue
@@ -59,7 +59,6 @@ export default {
 	left: 0;
 	width: 100%;
 	height: 100%;
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	color: #2c3e50;
