1fe39efe131f105548ee22aee02c4b20bc27c559
kisstest <kisstest2@gmail.com>
2020-08-08 15:48:34 +0900

    .

--- src/main.js
+++ src/main.js
@@ -31,27 +31,24 @@ import Api from '@/api';
 import customPlugin from '@/plugins/custom.js';
 // ----------------------------------------------------------------
 
-Vue.use(
-	Container,
-	Row,
-	Col,
-	Header,
-	Button,
-	Autocomplete,
-	Tooltip,
-	Avatar,
-	Image,
-	Drawer,
-	Menu,
-	MenuItem,
-	Main,
-	Card,
-	Form,
-	FormItem,
-	Input,
-	PageHeader,
-);
-
+Vue.use(Container);
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Header);
+Vue.use(Button);
+Vue.use(Autocomplete);
+Vue.use(Tooltip);
+Vue.use(Avatar);
+Vue.use(Image);
+Vue.use(Drawer);
+Vue.use(Menu);
+Vue.use(MenuItem);
+Vue.use(Main);
+Vue.use(Card);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Input);
+Vue.use(PageHeader);
 Vue.use(customPlugin);
 
 Vue.config.productionTip = false;
