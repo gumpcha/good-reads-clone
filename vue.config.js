cdff4831bda6302661839a460b908b371585bfac
kisstest <kisstest2@gmail.com>
2020-08-27 16:16:06 +0900

    ..

--- vue.config.js
+++ vue.config.js
@@ -16,38 +16,38 @@ module.exports = {
 		performance: {
 			hints: false,
 		},
-		optimization: {
-			splitChunks: {
-				cacheGroups: {
-					vendor: {
-						chunks: 'initial',
-						name: 'vendor',
-						enforce: true,
-					},
-					// commons: {
-					// 	test: /[\\/]node_modules[\\/]/,
-					// 	name: 'vendors',
-					// 	enforce: true,
-					// },
-					// commons: {
-					// 	name: 'vendor',
-					// 	test: /[\\/]node_modules[\\/]/,
-					// 	minChunks: 2,
-					// },
-					// shared: {
-					// 	test: /[\\/]node_modules[\\/]/,
-					// 	name: 'vendor',
-					// 	enforce: true,
-					// },
-				},
-			},
-			// 	splitChunks: {
-			// 		minSize: 10000,
-			// 		maxSize: 250000,
-			// 		chunks: 'all',
-			// 	},
-			// 	usedExports: true,
-		},
+		// optimization: {
+		// 	splitChunks: {
+		// 		cacheGroups: {
+		// 			vendor: {
+		// 				chunks: 'initial',
+		// 				name: 'vendor',
+		// 				enforce: true,
+		// 			},
+		// 			// commons: {
+		// 			// 	test: /[\\/]node_modules[\\/]/,
+		// 			// 	name: 'vendors',
+		// 			// 	enforce: true,
+		// 			// },
+		// 			// commons: {
+		// 			// 	name: 'vendor',
+		// 			// 	test: /[\\/]node_modules[\\/]/,
+		// 			// 	minChunks: 2,
+		// 			// },
+		// 			// shared: {
+		// 			// 	test: /[\\/]node_modules[\\/]/,
+		// 			// 	name: 'vendor',
+		// 			// 	enforce: true,
+		// 			// },
+		// 		},
+		// 	},
+		// 	// 	splitChunks: {
+		// 	// 		minSize: 10000,
+		// 	// 		maxSize: 250000,
+		// 	// 		chunks: 'all',
+		// 	// 	},
+		// 	// 	usedExports: true,
+		// },
 		plugins: [
 			new CleanWebpackPlugin(),
 			new MiniCssExtractPlugin({
